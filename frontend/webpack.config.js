d797d520fcc35098c1f608499f12042649109bc2
MaksymPastukh <saponexamq@gmail.com>
2024-03-27 20:00:51 +0100

    Income & expense create and editing

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: "./src/templates/signup.html"
     }),
     new CopyPlugin({
       patterns: [

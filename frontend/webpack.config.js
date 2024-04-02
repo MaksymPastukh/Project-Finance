51952105b11da0d400f9d8545803959537c18c8b
MaksymPastukh <saponexamq@gmail.com>
2024-04-02 18:46:39 +0200

    The Final Version of the Adaptive

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: "./src/templates/login.html"
     }),
     new CopyPlugin({
       patterns: [
